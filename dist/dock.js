7a662163181cf6a8feca7527bc7d8fcfdb850458
Raymond <ray@firestitch.com>
2017-07-29 17:23:49 -0400

    initial commit

--- dist/dock.js
+++ dist/dock.js
@@ -1,3 +1,22 @@
+
+(function () {
+    'use strict';
+
+    angular.module('fs-angular-dock',[])
+    .directive('fsDock', function($location) {
+        return {
+            templateUrl: 'views/directives/template.html',
+            restrict: 'E',
+            transclude: true,
+            scope: {
+               selected: "@fsSelected"
+            },
+            link: function($scope, element, attrs) {
+
+            }
+        };
+    });
+})();
 (function () {
     'use strict';
 
@@ -63,3 +82,11 @@
         }
     });
 })();
+angular.module('fs-angular-dock').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('views/directives/template.html',
+    "fs-angular template"
+  );
+
+}]);
--- src/app/scripts/controllers/demo.js
+++ src/app/scripts/controllers/demo.js
@@ -1,13 +1,26 @@
 'use strict';
-
-
 angular.module('app')
-  .controller('DemoCtrl', function ($scope) {
+  .controller('DemoCtrl', function ($scope, fsDock, $q) {
 
-    $scope.text = '';
+    $scope.show = function() {
 
-    $scope.submit = function() {
-        alert('submit');
+	    fsDock.show(
+	    {
+	    	template: '<div layout="row" layout-align="start center"><span flex>{{message}}</span><md-button ng-click="hide()">Hide</md-button></div>',
+	    	title: 'Dock Title',
+	    	controller: ['$scope','message',function($scope, message) {
+	    		$scope.message = message;
+	    	}],
+	    	resolve: {
+	    		message: function() {
+	    			return $q(function(resolve) {
+	    				resolve('How do you like me now?');
+	    			});
+	    		}
+	    	}
+
+	    });
+
     }
 });
 
